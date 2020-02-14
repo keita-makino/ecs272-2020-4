2f31c33443ad0661731d3f4fea635c812fb43526
keita-makino <kemakino@ucdavis.edu>
2020-02-13 23:45:10 -0800

    Updates

---
+++ precache-manifest.5a8204048a24e74f14aa053b824df600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63a8df0060fd5fdbdb2b7e82dfcfd4b",
+    "url": "/ecs272-2020-4/index.html"
+  },
+  {
+    "revision": "5d3d907f11c77b250c47",
+    "url": "/ecs272-2020-4/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "83e39643d12706045aa6",
+    "url": "/ecs272-2020-4/static/css/main.bf58a895.chunk.css"
+  },
+  {
+    "revision": "5d3d907f11c77b250c47",
+    "url": "/ecs272-2020-4/static/js/2.d45ec77d.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/ecs272-2020-4/static/js/2.d45ec77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e39643d12706045aa6",
+    "url": "/ecs272-2020-4/static/js/main.8a972eca.chunk.js"
+  },
+  {
+    "revision": "4009e079b9fa8585f556",
+    "url": "/ecs272-2020-4/static/js/runtime-main.33d14ce4.js"
+  }
+]);
